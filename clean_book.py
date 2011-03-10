1d2941d3761fff702c9b0ebe7644fc76ee34891b
Hugoagogo <Hughpowell2004@yahoo.com.au>
2011-03-10 11:37:35 +1100

    Changed to work on zip files exported from calibre

--- clean_book.py
+++ clean_book.py
@@ -1,4 +1,8 @@
 import re
+import shutil
+import tempfile
+import zipfile
+import os
 import Tkinter, tkFileDialog
 
 letters = "abcdefghijklmnopqrstuvwxyz"
@@ -14,17 +18,17 @@ def load_words(filename):
     return words
 
 def compound(word):
-    if 2 <= len(word)-1:
+    if 1 <= len(word) and len(word) > 5:
         if word[0] in letters and not (word.lower() in WORD_DICT or word.lower().rstrip("s") in WORD_DICT):
-            for x in xrange(1,len(word)-1):
+            for x in xrange(1,len(word)):
                 p1 = word[:x]
                 p2 = word[x:]
-                if len(p1) + len(p2) != 6:
-                    if p1.lower() in WORD_DICT and p2.lower() in WORD_DICT:
-                        if p2[0].lower() == "s" and p2[1:].lower() in WORD_DICT:
-                            p1+=p2[0]
-                            p2 = p2[1:]
-                        return p1+" "+p2
+##                print p1,p2
+                if p1.lower() in WORD_DICT and p2.lower() in WORD_DICT:
+                    if p2[0].lower() == "s" and p2[1:].lower() in WORD_DICT:
+                        p1+=p2[0]
+                        p2 = p2[1:]
+                    return p1+" "+p2
 
 def clean_book(data):
     corrections = 0
@@ -56,19 +60,53 @@ def clean_text(data,corrections=0):
     out = "".join(words)
     return out,corrections
 
+def remove_from_zip(zipfname, out_zipfname, *filenames):
+    tempdir = tempfile.mkdtemp()
+    try:
+        tempname = os.path.join(tempdir, 'new.zip')
+        zipread = zipfile.ZipFile(zipfname, 'r')
+        zipwrite = zipfile.ZipFile(tempname, 'w')
+        for item in zipread.infolist():
+            if item.filename not in filenames:
+                data = zipread.read(item.filename)
+                zipwrite.writestr(item, data)
+        zipread.close()
+        zipwrite.close()
+        shutil.move(tempname, out_zipfname)
+    finally:
+        shutil.rmtree(tempdir)
 
 regex_get_blocks = re.compile("(<.*?>)")
 regex_get_words = re.compile("([^a-zA-Z]*)")
-##regex_get_words = re.compile("((?:[^a-zA-Z])*|(?:<.*?>))")#
 WORD_DICT = set(load_words('words.txt'))
 
 root = Tkinter.Tk()
 root.withdraw()
-f = tkFileDialog.askopenfile(mode="rb",title="Open Text")
+in_zip_path = tkFileDialog.askopenfilename(title="Open Text")
+out_zip_path = tkFileDialog.asksaveasfilename(title="Save Text")
+
+zipy = zipfile.ZipFile(in_zip_path,"r")
+
+regex = re.compile(r"calibre.*?[/].*?[.]html")
+
+for name in zipy.namelist():
+    if re.match(regex,name):
+        break
+
+fileinfo = zipy.getinfo(name)
+f = zipy.open(name,"r")
 data = f.read()
 f.close()
-f = tkFileDialog.asksaveasfile(mode="w",title="Save Text")
-f.write(clean_book(data))
-f.close()
+zipy.close()
+
+
+cleaned_book = clean_book(data)
+
+remove_from_zip(in_zip_path,out_zip_path,name)
+
+zipy = zipfile.ZipFile(out_zip_path,"a")
+zipy.writestr(fileinfo,cleaned_book)
+zipy.close()
+
 ##root.quit()
 raw_input("Press enter to quit")
